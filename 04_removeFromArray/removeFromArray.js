95658f1df3fda4465a8d5a61bf2ee790dd9f4caf
baarayy <baraaahmed728@gmail.com>
2023-08-10 15:40:43 +0200

    Solved Task4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,22 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(myArray) {
+    let resultArray = [];
+    let stays = [];
+    for(let i = 0 ; i < myArray.length ; i++) stays.push(true);
+    for(let j = 1 ; j < arguments.length ; j++) {
+        let badElement = arguments[j];
+        for(let i = 0 ; i < myArray.length ; i++) {
+            if(myArray[i] === badElement) {
+                stays[i] = false;
+            }
+        }
+    }
+    for(let i = 0 ; i < myArray.length ; i++) {
+        if(stays[i]) {
+            resultArray.push(myArray[i]);
+        }
+    }
+    return resultArray;
 };
-
+console.log(removeFromArray([1, 2, 3], "1", 3));
 // Do not edit below this line
 module.exports = removeFromArray;
