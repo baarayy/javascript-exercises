c7207617d926bc8160919090c09f560a1c676904
baarayy <baraaahmed728@gmail.com>
2023-08-10 20:03:31 +0200

    Solved Task8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,27 +1,43 @@
-const add = function() {
-	
+const add = function(a , b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a , b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let result = 0;
+	array.forEach(element => {
+    result += element;
+  });
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let result = 1;
+  array.forEach(element => {
+    result *= element;
+  });
+  return result;
 };
 
-const power = function() {
-	
+const power = function(base , power) {
+  let result = 1;
+  while(power > 0) {
+    if(power % 2) result *= base;
+    power /= 2;
+    power = parseInt(power);
+    base *= base;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(fact) {
+	let result = 1;
+  for(let i = 1 ; i <= fact ; i++) result *= i;
+  return result;
 };
-
 // Do not edit below this line
 module.exports = {
   add,
