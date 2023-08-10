5588058f44956a7dc4525ce62829920602c697be
baarayy <baraaahmed728@gmail.com>
2023-08-10 15:13:39 +0200

    Solved Task2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(word ,repeats) {
+    let result = "";
+    if(word === "") return result;
+    if(repeats < 0) return "ERROR";
+    for(let i = 0 ; i < repeats ; i++) result += word;
+    return result;
 };
-
 // Do not edit below this line
 module.exports = repeatString;
