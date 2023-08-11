8e8d0e3e162cf88313b925de212e2635d3dc8149
baarayy <baraaahmed728@gmail.com>
2023-08-11 09:36:09 +0200

    Solved Task10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(fib) {
+    fib = parseInt(fib);
+    if(fib < 0) return "OOPS"
+    let dp = [0 , 1];
+    for(let i = 0 ; i < 50 ; i++) {
+        dp.push(dp[dp.length - 2] + dp[dp.length - 1]);
+    }
+    return dp[fib];
 };
-
 // Do not edit below this line
 module.exports = fibonacci;
