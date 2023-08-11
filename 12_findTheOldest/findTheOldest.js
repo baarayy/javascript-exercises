a2cbe6117b2d602684bae53a29a3ca2fa881de9d
baarayy <baraaahmed728@gmail.com>
2023-08-11 11:27:41 +0200

    Solved Task12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,38 @@
-const findTheOldest = function() {
-
-};
-
+const findTheOldest = function(people) {
+     
+    for(const person of people) {
+        if(!person.hasOwnProperty("yearOfDeath")) {
+            person.yearOfDeath = new Date().getFullYear();
+        }
+    }
+    let oldestPerson;
+    let oldest = 0;
+    for(const person of people) {
+        let age = person.yearOfDeath - person.yearOfBirth;
+        if(age > oldest) {
+            oldest = age;
+            oldestPerson = person;
+        }
+    }
+    return oldestPerson;
+}
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1942,
+      yearOfDeath: 1970,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
+console.log(findTheOldest(people));
 // Do not edit below this line
 module.exports = findTheOldest;
